0885fe6279cf20e52287e44884840302abf3c06e
Noel Escobedo <noel@michelada.io>
2018-08-13 05:55:47 +0000

    Separate index to TripsList and Trip

--- components/Home/Trip.js
+++ components/Home/Trip.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { Button, Text } from 'native-base';
+import { View, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
+import { Button, Text, Container, Content } from 'native-base';
 import openMap from 'react-native-open-maps';
 import mapsIcon from '../../assets/maps-icon.png'
+import Header from './Header';
 import Api from '../../utils/api';
+import firebase from 'firebase';
+import firebaseConfig from '../../firebaseconfig.json';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -54,13 +61,22 @@ class Trip extends React.Component {
     id: null,
     address: '',
     since: '',
-    full_name: ''
+    full_name: '',
+    refreshing: false,
+    status: '',
   }
 
   componentDidMount() {
+    this.getActiveTrip();
+  }
+  
+  getActiveTrip = () => {
+    // this.setState({ refreshing: true });
+    
     Api.get('/drivers/active_trip')
       .then(res => {
         this.setState({
+          refreshing: false,
           id: res.data.trip.id,
           address: res.data.trip.address_origin,
           since: '00:06',
@@ -68,7 +84,43 @@ class Trip extends React.Component {
           longitude: res.data.trip.lng_origin,
           full_name: res.data.trip.user.full_name
         })
+      }).catch(err => {
+        // this.setState({ refreshing: false });
+        console.log(err.response);
+        alert('No se pudo obtener la informacion del servicio');
       })
+  }
+
+  finishTrip = () => {
+    Api.put('/drivers/finish_trip')
+      .then(res => {
+        if (res.status == 200) {
+          this.props.setStatus('free');
+        } else {
+          console.log(res);
+        }
+      })
+  }
+
+  cancelTrip = () => {
+    Alert.alert(
+      'Cancelar',
+      '¿Está seguro que desea cancelar el servicio?',
+      [
+        {text: 'No', onPress: () => {}, style: 'cancel'},
+        {text: 'Si', onPress: () => 
+          Api.put('/drivers/cancel_trip')
+            .then(res => {
+              this.props.setStatus('free');
+            })
+            .catch(err => {
+              console.log(err.response);
+              alert('Ha ocurrido un error');
+            })
+        },
+      ],
+      { cancelable: false }
+    );
   }
 
   showMap = () => {
@@ -79,45 +131,53 @@ class Trip extends React.Component {
   render() {
     const { address, since, full_name } = this.state;
     const { status } = this.props;
+    const headerProps = {
+      status,
+      navigation: this.props.navigation,
+      cancelTrip: this.cancelTrip
+    };
     return (
-      <View style={styles.container}>
-        <View style={styles.labelWrapper}>
-          <Text style={styles.label}>Usuario</Text>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text style={styles.text}>{full_name}</Text>
+      <Container contentContainerStyle={{flex: 1}}>
+        <Header {...headerProps}/>
+        <Content contentContainerStyle={{flex: 1}}>
+          <View style={styles.labelWrapper}>
+            <Text style={styles.label}>Usuario</Text>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Text style={styles.text}>{full_name}</Text>
+            </View>
           </View>
-        </View>
 
-        <View style={styles.labelWrapper}>
-          <Text style={styles.label}>Dirección</Text>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text style={styles.text}>{address}</Text>
-            <TouchableOpacity onPress={this.showMap} style={{height: 70, width: 70}}>
-              <Image style={{ height: '100%', width: '100%' }} source={mapsIcon} />
-            </TouchableOpacity>
+          <View style={styles.labelWrapper}>
+            <Text style={styles.label}>Dirección</Text>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Text style={styles.text}>{address}</Text>
+              <TouchableOpacity onPress={this.showMap} style={{height: 70, width: 70}}>
+                <Image style={{ height: '100%', width: '100%' }} source={mapsIcon} />
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-        <View style={{ marginTop: 10 }}>
-          <Text style={styles.label}>Tiempo de espera</Text>
-          <Text style={styles.time}>{since}</Text>
-        </View>
+          <View style={{ marginTop: 10 }}>
+            <Text style={styles.label}>Tiempo de espera</Text>
+            <Text style={styles.time}>{since}</Text>
+          </View>
 
-        {status == 'taken' &&
-          <View style={styles.buttonWrapper}>
-            <Button large full primary style={styles.finishButton} onPress={this.props.startTrip}>
-              <Text style={styles.finishButtonText}>Iniciar servicio</Text>
-            </Button>
-          </View>
-        }
+          {status == 'taken' &&
+            <View style={styles.buttonWrapper}>
+              <Button large full primary style={styles.finishButton} onPress={this.startTrip}>
+                <Text style={styles.finishButtonText}>Iniciar servicio</Text>
+              </Button>
+            </View>
+          }
 
-        {status == 'active' &&
-          <View style={styles.buttonWrapper}>
-            <Button large full danger style={styles.finishButton} onPress={this.props.finishTrip}>
-              <Text style={styles.finishButtonText}>Finalizar servicio</Text>
-            </Button>
-          </View>
-        }
-      </View>
+          {status == 'active' &&
+            <View style={styles.buttonWrapper}>
+              <Button large full danger style={styles.finishButton} onPress={this.finishTrip}>
+                <Text style={styles.finishButtonText}>Finalizar servicio</Text>
+              </Button>
+            </View>
+          }
+        </Content>
+      </Container>
     );
   }
 }
--- components/Home/index.js
+++ components/Home/index.js
@@ -1,62 +1,28 @@
 import React from 'react';
 import {
   Alert,
-  FlatList,
   StyleSheet,
   View,
   PermissionsAndroid,
   Platform,
-  RefreshControl,
   Text
 } from 'react-native';
 import {
   Container,
   Content,
 } from 'native-base';
-import TripItem from './TripItem';
 import Trip from './Trip';
+import TripList from './TripsList';
 import Api from '../../utils/api';
-import Header from './Header';
-import geodist from 'geodist';
-import firebase from 'firebase';
-import firebaseConfig from '../../firebaseconfig.json';
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
 
 class Home extends React.Component {
 
   state = {
     status: 'free',
     currentTripId: null,
-    trips: [],
-    refreshing: false,
   }
 
   componentDidMount() {
-    // Solo debería traerme los trips que se generaron despues del componentDidMount
-    firebase.database().ref('server/holding_trips/').on('child_added', (snapshot) => {
-      if (snapshot.val()) {
-        // this.compareWithCurrentPosition(trip)
-        this.setState({
-          trips: [...this.state.trips, snapshot.val()]
-        });
-      } else {
-        this.setState({ trips: [] });
-      }
-    });
-
-    firebase.database().ref('server/holding_trips/').on('child_removed', (snapshot) => {
-      if (snapshot.val()) {
-        this.setState({
-          trips: this.state.trips.filter(item => item.id != snapshot.val().id)
-        });
-      } else {
-        this.setState({ trips: [] });
-      }
-    });
-
     Api.get('/drivers/active_trip')
       .then(res => {
         if (res.data && res.data.active) {
@@ -64,8 +30,6 @@ class Home extends React.Component {
             status: res.data.trip.status,
             currentTripId: res.data.trip.id
           });
-        } else {
-          this.getHoldingTrips();
         }
       }).catch(err => {
         if (err.response.status == 401) {
@@ -88,180 +52,27 @@ class Home extends React.Component {
     });
   }*/
 
-  currentPosition = () => {
-    return new Promise((resolve, reject) => {
-      this.checkGPSPermissions().then(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-          let { latitude, longitude } = position.coords;
-          return resolve({ lat: latitude, lon: longitude });
-        },
-        (error) => this.setState({ error: error.message }, () => reject(error)),
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 })
-      }).catch(err => {
-        console.log('Gps permissions', err);
-        return reject(err);
-      })
-    })
-  }
-
-  compareWithCurrentPosition = (trip)  => {
-    const geodistOptions = { exact: true, unit: 'km' };
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        let { latitude, longitude } = position.coords;
-        let origin_coords = {lat: latitude, lon: longitude};
-        let destiny_coords = {lat: trip.lat_origin, lon: trip.lng_origin};
-        let distance = geodist(origin_coords, destiny_coords, geodistOptions)
-        if (distance <= 4){
-          this.setState({
-            trips: [...this.state.trips, trip]
-          })
-        }
-      },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
-  }
-
-  checkGPSPermissions = () => {
-    return new Promise((resolve, reject) => {
-      Platform.select({
-        ios: () => resolve(),
-        android: () => {
-          PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
-            .then(granted => {
-              if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                return resolve()
-              } else {
-                this.setState({ error: 'Se requieren permisos de ubicación' })
-                return reject({ error: 'Se requieren permisos de ubicación' });
-              }
-            });
-        }
-      })();
-    })
-  }
-
-  getHoldingTrips = () => {
-    this.currentPosition().then(({ lat, lng }) => {
-      Api.post('/drivers/trips_in_range', { lat, lng })
-        .then(res => {
-          if (Array.isArray(res.data)) {
-            this.setState({
-              trips: res.data,
-              status: 'free',
-              currentTripId: null
-            });
-          }
-        }).catch(err => {
-          console.log('Trips catch', err.response)
-        })
-    }).catch(err => {
-      console.log('No se pudo obtener la ubicacion', err)
-    })
-  }
-
-  _keyExtractor = (item, index) => `${item.id}`;
-
-  renderSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: "95%",
-          backgroundColor: "#CED0CE",
-          marginLeft: 10
-        }}
-      />
-    );
-  }
-
-  takeTrip = (id, address) => {
-    Alert.alert(
-      'Nuevo servicio',
-      `¿Tomar viaje a ${address}?`,
-      [
-        {text: 'No', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-        {text: 'Si', onPress: () =>
-        Api.put('/drivers/accept_trip', { trip_id: id }).then(res => {
-          if (res.status == 200) {
-            this.setState({
-              status: 'active',
-              currentTripId: id
-            })
-          }
-        })
-        },
-      ],
-      { cancelable: false }
-    );
-  }
-
-  finishTrip = () => {
-    Api.put('/drivers/finish_trip')
-      .then(res => {
-        if (res.status == 200) {
-          this.getHoldingTrips();
-        } else {
-          console.log(res);
-        }
-      })
-  }
-
-  cancelTrip = () => {
-    Alert.alert(
-      'Cancelar',
-      '¿Está seguro que desea cancelar el servicio?',
-      [
-        {text: 'No', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-        {text: 'Si', onPress: () => 
-          Api.put('/drivers/cancel_trip')
-            .then(res => {
-              this.getHoldingTrips();
-            })
-            .catch(err => {
-              console.log(err.response);
-              alert('Ha ocurrido un error');
-            })
-        },
-      ],
-      { cancelable: false }
-    );
+  setStatus = (status) => {
+    this.setState({ status });
   }
 
   render() {
-    const { trips, status } = this.state;
-    const headerProps = {
-      status,
-      cancelTrip: this.cancelTrip,
-      navigation: this.props.navigation
-    };
-
-    return (
-      <Container contentContainerStyle={{flex: 1}}>
-        <Header {...headerProps}/>
-        {status === 'free' &&
-          <FlatList
-            data={trips}
-            keyExtractor={this._keyExtractor}
-            ItemSeparatorComponent={this.renderSeparator}
-            renderItem={({item}) => <TripItem key={item.id} takeTrip={this.takeTrip} status={status} {...item} />}
-            refreshControl={
-              <RefreshControl
-               refreshing={this.state.refreshing}
-               onRefresh={this.getHoldingTrips}
-              />
-            }
+    const { status } = this.state;
+      if (status === 'free') {
+        return (
+          <TripList
+            navigation={this.props.navigation}
+            setStatus={this.setStatus} />
+        )
+      } else {
+        return (
+          <Trip
+            status={status}
+            navigation={this.props.navigation}
+            setStatus={this.setStatus}
           />
-        }
-        {['taken', 'inprogress', 'active'].includes(status) &&
-          <Content contentContainerStyle={{flex: 1}}>
-            <Trip status={status} finishTrip={this.finishTrip} cancelTrip={this.cancelTrip} startTrip={this.startTrip} />
-          </Content>
-        }
-      </Container>
-    )
+        )
+      }
   }
 }
 
