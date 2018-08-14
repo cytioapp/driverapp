f0c23127fd888adc34b94077a50e765b80b1018c
Myrka Larios <myrka@michelada.io>
2018-08-14 14:05:10 -0500

    Changes loading screen style

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  View
+} from 'react-native';
 import { Spinner } from 'native-base';
+import logo2 from '../../assets/logo2.png';
+import fondo2 from '../../assets/fondo2.jpg';
+const window = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
+    height: window.height,
+    width: '100%'
+  },
+  logoContainer: {
+    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: 'flex-end'
+  },
+  logoImage: {
+    height: 159,
+    width: 190
+  },
+  container: {
+    justifyContent: 'space-between',
+    flex: 1
   }
 });
 
 const Loading = () => {
   return (
-    <View style={styles.container}>
-      <Text>Espere un momento</Text>
-      <Spinner />
-    </View>
+    <ImageBackground source={fondo2} style={styles.backgroundImage}>
+      <View style={styles.logoContainer}>
+        <Image source={logo2} style={styles.logoImage}/>
+      </View>
+      <View style={styles.container}>
+        <View></View>
+        <Spinner color="#E3C463"/>
+        <View></View>
+      </View>
+    </ImageBackground>
   );
 }
 
