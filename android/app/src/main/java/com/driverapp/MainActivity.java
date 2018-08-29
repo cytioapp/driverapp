6be6845d63ac420ab264ae05c4da992ec94a5ef8
Noel Escobedo <noel@michelada.io>
2018-08-29 21:51:14 +0000

    Android namespace update

--- android/app/src/main/java/com/driverapp/MainActivity.java
+++ android/app/src/main/java/com/driverapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.driverapp;
+package com.cytiodriver;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "driverapp";
+        return "cytiodriver";
     }
 }
